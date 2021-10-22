1d84e5c3022b11e1c0127cd685e2dedb43398471
tf00185077 <tf00185077@gmail.com>
2021-10-22 11:10:08 +0800

    重新引入jquery

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,10 +1,21 @@
+
 <template>
 	<div>
-    	<p>asd
+    	<p @click="add">asd
         <span>ERT</span>
       </p>
     </div>
 </template>
+<script>
+import $ from 'jquery'
+export default {
+  methods: {
+    add(){
+      $("p").css("fontSize",50);
+    }
+  },
+}
+</script>
 <style lang="scss">
 @import './assets/style/var.scss';
 $maincolor:red;
