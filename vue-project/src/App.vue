7ee17809cbd04a2762a12cce810aff11bb4f7cae
melody0602 <melodywang88621@gmail>
2021-10-22 13:53:23 +0800

    vue test

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,8 +1,4 @@
 <template>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> c26eb22e1159b8e2b78aa1e13491be242443f354
   <div id="app">
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
@@ -21,11 +17,16 @@
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
   </div>
-<<<<<<< HEAD
 </template>
 
 <script>
+import $ from "jquery";
 export default {
+  // methods: {
+  //   add(){
+  //     $("h1").hide();
+  //   }
+  // },
   name: 'app',
   data () {
     return {
@@ -35,7 +36,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -62,56 +63,4 @@ li {
 a {
   color: #42b983;
 }
-=======
-    <div>
-        
-    </div>
-=======
->>>>>>> c26eb22e1159b8e2b78aa1e13491be242443f354
-</template>
-
-<script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
-</script>
-<<<<<<< HEAD
-<style scoped>
-    
->>>>>>> fcf5d306856dfbf73c37e0cc8b21fd5e487a6d43
-=======
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
->>>>>>> c26eb22e1159b8e2b78aa1e13491be242443f354
 </style>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -4,8 +4,4 @@ import App from './App.vue'
 new Vue({
   el: '#app',
   render: h => h(App)
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> fcf5d306856dfbf73c37e0cc8b21fd5e487a6d43
